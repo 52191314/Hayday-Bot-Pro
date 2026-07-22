96f28b554ee0eb64f068e0ba2b780a6c9b25a0d2
52191314 <xianspired@gmail.com>
2026-07-23 06:51:08 +0700

    Sanitize repository: remove Promon/headless/Frida artifacts and update README

--- magisk_memu_boot/scripts/test_raw_sda3_write.sh
+++
@@ -1,7 +0,0 @@
-#!/system/bin/sh
-set -e
-for m in $(awk '$1=="/dev/block/sda3"{print $2}' /proc/mounts); do umount "$m" 2>/dev/null || true; done
-dd if=/dev/block/sda3 of=/data/local/tmp/sda3_first4096.bin bs=4096 count=1
-dd if=/data/local/tmp/sda3_first4096.bin of=/dev/block/sda3 bs=4096 count=1 seek=0
-sync
-echo raw_write_status=$?
